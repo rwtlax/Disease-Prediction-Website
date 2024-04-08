fa53be49e7fc928c1c3aee01d2f19edce676ca6d
rwtlax <67238731+rwtlax@users.noreply.github.com>
2024-04-08 18:04:28 +0530

    settings.py

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'v3v5vfsn0xxjtmb=eoawoiw$5br4g0r&jy_l39995h_93l+-z5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
